4a6612908fda7a94e88f8a53d1c7850b41fc36b0
Kenny Johnston <kencjohnston@gmail.com>
2016-09-18 21:47:53 -0500

    Add /kids to johnstonfamily

--- johnstonfamily/app.py
+++ johnstonfamily/app.py
@@ -23,31 +23,15 @@ KIDS_TO_PARENTS = {
 @app.route('/')
 def index():
     return {'hello': 'world'}
+
+@app.route('/kids')
+def index():
+    return {'kids':'%s' % (', '.join(KIDS_TO_PARENTS.keys()))}
+    
 @app.route('/kids/{parent}')
 def kids_of_parents(parent):
     return{'kids':PARENTS_TO_KIDS[parent]}
+
 @app.route('/parents/{kid}')
 def parents_of_kids(kid):
     return{'parents':KIDS_TO_PARENTS[kid]}
-
-# The view function above will return {"hello": "world"}
-# whenver you make an HTTP GET request to '/'.
-#
-# Here are a few more examples:
-#
-# @app.route('/hello/{name}')
-# def hello_name(name):
-#    # '/hello/james' -> {"hello": "james"}
-#    return {'hello': name}
-#
-# @app.route('/users/', methods=['POST'])
-# def create_user():
-#     # This is the JSON body the user sent in their POST request.
-#     user_as_json = app.json_body
-#     # Suppose we had some 'db' object that we used to
-#     # read/write from our database.
-#     # user_id = db.create_user(user_as_json)
-#     return {'user_id': user_id}
-#
-# See the README documentation for more exampes.
-#
